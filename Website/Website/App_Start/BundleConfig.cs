adf0074ba94f0d7907fa2d86bc608ee889d76a30
Andrew Foran <amforan@crimson.ua.edu>
2018-03-28 17:33:06 -0500

    Theme update

--- Website/Website/App_Start/BundleConfig.cs
+++ Website/Website/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace Website
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/united-bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
